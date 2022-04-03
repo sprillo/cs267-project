cc3a1149eef8bbf6e32f15fac0f268e439d3ac2b
Sebastián Prillo <10426884+sprillo@users.noreply.github.com>
2022-04-02 18:35:39 -0700

    imports

--- src/counting/count_co_transitions.py
+++ src/counting/count_co_transitions.py
@@ -6,8 +6,7 @@ import numpy as np
 import pandas as pd
 import tqdm
 
-from src.io import read_msa, read_tree, write_count_matrices
-from src.io.contact_map import read_contact_map
+from src.io import read_msa, read_tree, read_contact_map, write_count_matrices
 from src.utils import get_process_args, quantize
 
 
