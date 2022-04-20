d261062ee5cf65076e4ea630e15792881e2f12fc
xingyuli9961 <xingyuli9961@berkeley.edu>
2022-04-20 15:56:19 -0700

    added some commented lines in the simulation_test.py to generate some input files for testing when developing C++ files

--- src/simulation/simulate.cpp
+++ src/simulation/simulate.cpp
@@ -1,4 +1,38 @@
-// This is the file contains the C++ implementation of the simulation function
+/**
+ * @file simulate.cpp
+ * @author Xingyu Li (xingyuli9961@berkeley.edu)
+ * @brief This is the file contains the C++ implementation of the simulation function
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * Prerequisite:
+ * module load openmpi
+ * Compile the code:
+ * mpicxx -o simulate simulate.cpp
+ * An example testing command use for developing and checking if the file compiles:
+ * ./simulate ./../../tests/simulation_tests/test_input_data/tree_dir
+ * 
+ * Below shows the testing arguments during development:
+ * (This is a sample version of the test_simulate_msas_normal_model)
+ * argv[1]  (tree_dir): "./../../tests/simulation_tests/test_input_data/tree_dir"
+ * argv[2]  (site_rates_dir): "./../../tests/simulation_tests/test_input_data/synthetic_site_rates_dir"
+ * argv[3]  (contact_map_dir): "./../../tests/simulation_tests/test_input_data/synthetic_contact_map_dir"
+ * argv[4]  (num_of_families): 3
+ * argv[5]  (num_of_amino_acids): 2
+ * argv[6]  (pi_1_path): "./../../tests/simulation_tests/test_input_data/normal_model/pi_1.txt"
+ * argv[7]  (Q_1_path): "./../../tests/simulation_tests/test_input_data/normal_model/Q_1.txt"
+ * agrv[8]  (pi_2_path): "./../../tests/simulation_tests/test_input_data/normal_model/pi_2.txt"
+ * argv[9]  (Q_2_path): "./../../tests/simulation_tests/test_input_data/normal_model/Q_2.txt"
+ * argv[10] (strategy): "all_transitions"
+ * argv[11] (output_msa_dir): "./../../tests/simulation_tests/test_input_data/simulated_msa_dir"
+ * argv[12] (random_seed): 0
+ * argv[13] (num_processes): 3
+ * argv[14 : 17] (families): ["fam1", "fam2", "fam3"]
+ * argv[17 : 19] (amino_acids): ["S", "T"]
+ * 
+ */
 #include <chrono>
 #include <cmath>
 #include <cstring>
@@ -10,5 +44,9 @@
 
 int main(int argc, char *argv[]) {
     std::cout << "This is the start of this testing file ..." << std::endl;
-    
+    std::string tree_dir = argv[1];
+    std::string site_rates_dir = argv[2];
+    std::string contact_map_dir = argv[3];
+    int num_family = argv[4]
+    std::cout << tree_dir << std::endl;
 }
--- tests/simulation_tests/simulation_test.py
+++ tests/simulation_tests/simulation_test.py
@@ -171,6 +171,8 @@ class TestSimulation(unittest.TestCase):
         families = ["fam1", "fam2", "fam3"]
         tree_dir = "./tests/simulation_tests/test_input_data/tree_dir"
         with tempfile.TemporaryDirectory() as synthetic_contact_map_dir:
+        # if True:
+        #     synthetic_contact_map_dir = "./tests/simulation_tests/test_input_data/synthetic_contact_map_dir"
             # Create synthetic contact maps
             contact_maps = {}
             for i, family in enumerate(families):
@@ -188,6 +190,8 @@ class TestSimulation(unittest.TestCase):
                 contact_maps[family] = contact_map
 
             with tempfile.TemporaryDirectory() as synthetic_site_rates_dir:
+            # if True:
+            #     synthetic_site_rates_dir = "./tests/simulation_tests/test_input_data/synthetic_site_rates_dir"
                 for i, family in enumerate(families):
                     site_rates = [1.0 * np.log(1 + i) for i in range(num_sites)]
                     site_rates_path = os.path.join(
@@ -196,6 +200,8 @@ class TestSimulation(unittest.TestCase):
                     write_site_rates(site_rates, site_rates_path)
 
                 with tempfile.TemporaryDirectory() as simulated_msa_dir:
+                # if True:
+                #     simulated_msa_dir = "./tests/simulation_tests/test_input_data/simulated_msa_dir"
                     simulate_msas(
                         tree_dir=tree_dir,
                         site_rates_dir=synthetic_site_rates_dir,
