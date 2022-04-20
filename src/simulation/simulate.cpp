2e3dbcfaa35c28ba7e196f89d1e7c9970a943de6
xingyuli9961 <xingyuli9961@berkeley.edu>
2022-04-20 11:40:45 -0700

    An initial commit with a sample testing file to start this branch

---
+++ src/simulation/simulate.cpp
@@ -0,0 +1,14 @@
+// This is the file contains the C++ implementation of the simulation function
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    std::cout << "This is the start of this testing file ..." << std::endl;
+    
+}
