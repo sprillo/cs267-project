c48e46bb1d8f4926b3c0f36c99294be91d055c40
Sebastian Prillo <sprillo@otus.EECS.Berkeley.EDU>
2022-04-24 10:56:56 -0700

    Add FastTree

--- src/phylogeny_estimation/_fast_tree.py
+++ src/phylogeny_estimation/_fast_tree.py
@@ -1,20 +1,246 @@
-from typing import List
-
 import logging
+import os
+import tempfile
+import time
+from typing import List, Tuple
 
-# PhylogenyGenerator(
-#     a3m_dir_full=a3m_dir_full,
-#     a3m_dir=a3m_dir,
-#     n_process=n_process,
-#     expected_number_of_MSAs=expected_number_of_MSAs,
-#     outdir=tree_dir,
-#     max_seqs=max_seqs,
-#     max_sites=max_sites,
-#     max_families=max_families,
-#     rate_matrix=rate_matrix,
-#     fast_tree_cats=fast_tree_cats,
-#     use_cached=use_cached,
-# )
+import numpy as np
+import pandas as pd
+from ete3 import Tree
+
+from src.io import read_rate_matrix
+from src.markov_chain import compute_stationary_distribution
+
+
+def get_rate_categories(
+    tree_dir: str,
+    protein_family_name: str,
+    use_site_specific_rates: bool,
+    L: int,
+) -> Tuple[List[float], List[int], List[int]]:
+    """
+    Returns the rates, site_cats, sites_kept for the given protein
+    family.
+
+    If use_site_specific_rates=False, it is assumed that all sites evolve
+    at the same rate of 1.
+
+    Args:
+        tree_dir: Directory where the FastTree log is found.
+        protein_family_name: Protein family name.
+        use_site_specific_rates: If to use_site_specific_rates. If False, will
+            just use a rate of 1 for all sites, which is the old behaviour.
+            I.e. we are backwards compatible here.
+        L: Number of sites. Used only if use_site_specific_rates=False.
+    """
+    outlog = os.path.join(tree_dir, protein_family_name + ".log")
+    with open(outlog, "r") as outlog_file:
+        lines = [line.strip().split() for line in outlog_file]
+        if not lines[0][0] == "NCategories":
+            raise ValueError(f"NCategories not found in {outlog}")
+        ncats = int(lines[0][1])
+        if not lines[1][0] == "Rates":
+            raise ValueError(f"Rates not found in {outlog}")
+        if not len(lines[1][1:]) == ncats:
+            raise ValueError(
+                f"Rates should be {ncats} in length. Found {len(lines[1][1:])}."
+            )
+        rates = [float(x) for x in lines[1][1:]]
+        if not lines[2][0] == "SiteCategories":
+            raise ValueError(f"SiteCategories not found in {outlog}")
+        # FastTree uses 1-based indexing for SiteCategories, so we shift by 1.
+        site_cats = [int(x) - 1 for x in lines[2][1:]]
+    out_sites_kept_path = os.path.join(
+        tree_dir, protein_family_name + ".sites_kept"
+    )
+    with open(out_sites_kept_path, "r") as out_sites_kept_file:
+        sites_kept = [
+            int(x) for x in out_sites_kept_file.read().strip().split()
+        ]
+    if not len(site_cats) == len(sites_kept):
+        raise ValueError(
+            f"SiteCategories should have length {len(sites_kept)},"
+            f" but has {len(site_cats)} instead."
+        )
+    if not use_site_specific_rates:
+        # Just force all rates to 1, and use all sites, which is the old
+        # behavior. (So, we are backwards compatible.)
+        rates = [1.0 for _ in rates]
+        site_cats = [0] * L
+        sites_kept = list(range(L))
+    return rates, site_cats, sites_kept
+
+
+def get_site_rate_from_site_id(
+    site_id: int,
+    rates: List[float],
+    site_cats: List[int],
+    sites_kept: List[int],
+):
+    """
+    Precondition: the site_id must be in the sites_kept, or an error will be
+    raised.
+    """
+    if not len(site_cats) == len(sites_kept):
+        raise ValueError(
+            f"site_cats and sites_kept should have the same length."
+            f" len(site_cats) = {len(site_cats)}; "
+            f"len(sites_kept) = {len(sites_kept)}"
+        )
+    # Find its rate
+    for site, site_cat in zip(sites_kept, site_cats):
+        if site == site_id:
+            return rates[site_cat]
+    raise ValueError(
+        "Trying to retrieve rate for site that was not kept."
+        f" Site: {site_id}. sites_kept = {sites_kept}"
+    )
+
+
+def to_fast_tree_format(rate_matrix: np.array, output_path: str, pi: np.array):
+    r"""
+    The weird 20 x 21 format of FastTree, which is also column-stochastic.
+    """
+    amino_acids = [
+        "A",
+        "R",
+        "N",
+        "D",
+        "C",
+        "Q",
+        "E",
+        "G",
+        "H",
+        "I",
+        "L",
+        "K",
+        "M",
+        "F",
+        "P",
+        "S",
+        "T",
+        "W",
+        "Y",
+        "V",
+    ]
+    rate_matrix_df = pd.DataFrame(
+        rate_matrix, index=amino_acids, columns=amino_acids
+    )
+    rate_matrix_df = rate_matrix_df.transpose()
+    rate_matrix_df["*"] = pi
+    with open(output_path, "w") as outfile:
+        for aa in amino_acids:
+            outfile.write(aa + "\t")
+        outfile.write("*\n")
+        outfile.flush()
+    rate_matrix_df.to_csv(output_path, sep="\t", header=False, mode="a")
+
+
+def run_fast_tree_with_custom_rate_matrix(
+    msa_path: str,
+    family: str,
+    rate_matrix_path: str,
+    num_rate_categories: int,
+    output_tree_dir: str,
+) -> str:
+    r"""
+    This wrapper deals with the fact that FastTree only accepts normalized rate
+    matrices as input. Therefore, to run FastTree with an arbitrary rate matrix,
+    we first have to normalize it. After inference with FastTree, we have to
+    'de-normalize' the branch lengths to put them in the same time units as the
+    original rate matrix.
+    """
+    with tempfile.NamedTemporaryFile("w") as scaled_tree_file:
+        scaled_tree_filename = (
+            scaled_tree_file.name
+        )  # Where FastTree will write its output.
+        with tempfile.NamedTemporaryFile("w") as scaled_rate_matrix_file:
+            scaled_rate_matrix_filename = (
+                scaled_rate_matrix_file.name
+            )  # The rate matrix for FastTree
+            Q_df = read_rate_matrix(rate_matrix_path)
+            if not (Q_df.shape == (20, 20)):
+                raise ValueError(
+                    f"The rate matrix {rate_matrix_path} does not have "
+                    "dimension 20 x 20."
+                )
+            Q = np.array(Q_df)
+            pi = compute_stationary_distribution(Q)
+            # Check that rows (originally columns) of Q add to 0
+            if not np.sum(np.abs(Q.sum(axis=1))) < 0.01:
+                raise ValueError(
+                    f"Custom rate matrix {rate_matrix_path} doesn't have "
+                    "columns that add up to 0."
+                )
+            # Check that the stationary distro is correct
+            if not np.sum(np.abs(pi @ Q)) < 0.01:
+                raise ValueError(
+                    f"Custom rate matrix {rate_matrix_path} doesn't have the "
+                    "stationary distribution."
+                )
+            # Compute the mutation rate.
+            mutation_rate = pi @ -np.diag(Q)
+            # Normalize Q
+            Q_normalized = Q / mutation_rate
+            # Write out Q_normalized in FastTree format, for use in FastTree
+            to_fast_tree_format(
+                Q_normalized,
+                output_path=scaled_rate_matrix_filename,
+                pi=pi.reshape(20),
+            )
+            # Run FastTree!
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            outlog = os.path.join(output_tree_dir, family + ".fast_tree_log")
+            command = (
+                f"{dir_path}/FastTree -quiet -trans "
+                + f"{scaled_rate_matrix_filename} -log {outlog} -cat "
+                + f"{num_rate_categories} < {msa_path} > "
+                + f"{scaled_tree_filename}"
+            )
+            st = time.time()
+            os.system(command)
+            et = time.time()
+            open(os.path.join(output_tree_dir, family + ".command"), "w").write(
+                f"time_fast_tree: {et - st}"
+            )
+            # De-normalize the branch lengths of the tree
+            tree = Tree(scaled_tree_filename)
+
+            def dfs_scale_tree(v: tree) -> None:
+                for u in v.get_children():
+                    u.dist = u.dist / mutation_rate
+                    dfs_scale_tree(u)
+
+            dfs_scale_tree(tree)
+            tree.write(
+                format=2,
+                outfile=os.path.join(output_tree_dir, family + ".newick"),
+            )
+            open(os.path.join(output_tree_dir, family + ".command"), "w").write(
+                command
+            )
+            # TODO: Convert into our friendy format by using the
+            # write_tree function!
+
+
+def post_process_fast_tree_log(outlog: str):
+    """
+    We just want the sites and rates, so we prune the FastTree log file to keep
+    just this information.
+    """
+    res = ""
+    with open(outlog, "r") as infile:
+        for line in infile:
+            if (
+                line.startswith("NCategories")
+                or line.startswith("Rates")
+                or line.startswith("SiteCategories")
+            ):
+                res += line
+    with open(outlog, "w") as outfile:
+        outfile.write(res)
+        outfile.flush()
+
 
 # @cached_parallel_computation(
 #     exclude_args=["num_processes"],
@@ -24,22 +250,41 @@ import logging
 def fast_tree(
     msa_dir: str,
     families: List[str],
-    num_processes: int,
-    output_tree_dir: str,
     rate_matrix_path: str,
     num_rate_categories: int,
+    output_tree_dir: str,
+    num_processes: int,
 ) -> None:
     logger = logging.getLogger("rate_estimation.fast_tree")
 
-    dir_path = os.path.dirname(os.path.realpath(_file_))
-    c_path = os.path.join(dir_path, 'FastTree.c')
-    bin_path = os.path.join(dir_path, 'FastTree')
+    if num_processes > 1:
+        raise NotImplementedError
+
+    if not os.path.exists(output_tree_dir):
+        os.makedirs(output_tree_dir)
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    c_path = os.path.join(dir_path, "FastTree.c")
+    bin_path = os.path.join(dir_path, "FastTree")
     if not os.path.exists(bin_path):
         os.system(
-            f"wget http://www.microbesonline.org/fasttree/FastTree.c -P {dir_path}"
+            "wget http://www.microbesonline.org/fasttree/FastTree.c -P "
+            f"{dir_path}"
         )
-        compile_command = f"gcc -DNO_SSE -DUSE_DOUBLE -O3 -finline-functions -funroll-loops -Wall -o {bin_path} {c_path} -lm"
+        compile_command = (
+            "gcc -DNO_SSE -DUSE_DOUBLE -O3 -finline-functions -funroll-loops"
+            + f" -Wall -o {bin_path} {c_path} -lm"
+        )
         logger.info(f"Compiling FastTree with:\n{compile_command}")
         # See http://www.microbesonline.org/fasttree/#Install
         os.system(compile_command)
-    raise NotImplementedError
+
+    for family in families:  # TODO: Parallelize with Python multiprocessing!
+        msa_path = os.path.join(msa_dir, family + ".txt")
+        run_fast_tree_with_custom_rate_matrix(
+            msa_path=msa_path,
+            family=family,
+            rate_matrix_path=rate_matrix_path,
+            num_rate_categories=num_rate_categories,
+            output_tree_dir=output_tree_dir,
+        )
--- tests/phylogeny_estimation_tests/fast_tree_test.py
+++ tests/phylogeny_estimation_tests/fast_tree_test.py
@@ -1,16 +1,17 @@
 import os
-import unittest
 import tempfile
-from filecmp import dircmp
+import unittest
+
+from ete3 import Tree
 from parameterized import parameterized
 
 from src.phylogeny_estimation import fast_tree
 
-from ete3 import Tree
-
 
 def branch_length_l1_error(tree_true_path, tree_inferred_path) -> float:
+    print(f"Going to read: {tree_true_path}")
     tree1 = Tree(tree_true_path)
+    print(f"Going to read: {tree_inferred_path}")
     tree2 = Tree(tree_inferred_path)
 
     def dfs_branch_length_l1_error(v1, v2) -> float:
@@ -19,152 +20,127 @@ def branch_length_l1_error(tree_true_path, tree_inferred_path) -> float:
             l1_error += abs(u1.dist - u2.dist)
             l1_error += dfs_branch_length_l1_error(u1, u2)
         return l1_error
+
     l1_error = dfs_branch_length_l1_error(tree1, tree2)
     return l1_error
 
 
 class TestFastTree(unittest.TestCase):
-    def test_basic_regression(self):
+    @parameterized.expand([("single-process", 1)])
+    def test_basic_regression(self, name, num_processes):
         """
         Test that FastTree runs and its output matches the expected output.
         The expected output is located at test_input_data/trees_small
         """
-        with tempfile.TemporaryDirectory() as root_dir:
-            outdir = os.path.join(root_dir, 'trees_small')
+        with tempfile.TemporaryDirectory() as output_tree_dir:
+            # output_tree_dir = "fast_tree_trees_small"
             families = ["1e7l_1_A", "5a0l_1_A", "6anz_1_B"]
             fast_tree(
-                msa_dir='test_input_data/a3m_small',
+                msa_dir="tests/test_input_data/a3m_small_fast_tree",
                 families=families,
-                num_processes=n_process,
-                outdir=outdir,
-                rate_matrix='data/rate_matrices/wag.txt',
+                rate_matrix_path="data/rate_matrices/jtt.txt",  # TODO: Add JTT to GitHub!
                 num_rate_categories=20,
-                output_tree_dir=root_dir,
+                output_tree_dir=output_tree_dir,
+                num_processes=num_processes,
             )
             for protein_family_name in families:
-                tree_true_path = f"test_input_data/trees_small/{protein_family_name}.newick"
-                tree_inferred_path = os.path.join(outdir, protein_family_name + '.newick')
-                l1_error = branch_length_l1_error(tree_true_path, tree_inferred_path)
-                assert(l1_error < 0.02)  # Redundant, but just in case
+                tree_true_path = f"tests/test_input_data/trees_small/{protein_family_name}.newick"
+                tree_inferred_path = os.path.join(
+                    output_tree_dir, protein_family_name + ".newick"
+                )
+                l1_error = branch_length_l1_error(
+                    tree_true_path, tree_inferred_path
+                )
+                assert l1_error < 0.02  # Redundant, but just in case
 
-    # @parameterized.expand([("multiprocess", 3), ("single-process", 1)])
-    # def test_custom_rate_matrix_runs_regression(self, name, n_process):
-    #     """
-    #     Tests the use of a custom rate matrix in FastTree.
-    #     """
-    #     with tempfile.TemporaryDirectory() as root_dir:
-    #         outdir = os.path.join(root_dir, 'trees_small_Q1_uniform')
-    #         phylogeny_generator = PhylogenyGenerator(
-    #             a3m_dir_full='test_input_data/a3m_small',
-    #             a3m_dir='test_input_data/a3m_small',
-    #             n_process=n_process,
-    #             expected_number_of_MSAs=3,
-    #             outdir=outdir,
-    #             max_seqs=8,
-    #             max_sites=16,
-    #             max_families=3,
-    #             rate_matrix='input_data/synthetic_rate_matrices/Q1_uniform_FastTree.txt',
-    #             fast_tree_cats=20,
-    #             use_cached=False,
-    #         )
-    #         phylogeny_generator.run()
-    #         dcmp = dircmp(outdir, 'test_input_data/trees_small_Q1_uniform')
-    #         diff_files = dcmp.diff_files
-    #         assert(len(diff_files) == 0)
+    @parameterized.expand([("single-process", 1)])
+    def test_custom_rate_matrix_runs_regression(self, name, num_processes):
+        """
+        Tests the use of a custom rate matrix in FastTree.
+        """
+        with tempfile.TemporaryDirectory() as output_tree_dir:
+            # output_tree_dir = "fast_tree_trees_small"
+            families = ["1e7l_1_A", "5a0l_1_A", "6anz_1_B"]
+            fast_tree(
+                msa_dir="tests/test_input_data/a3m_small_fast_tree",
+                families=families,
+                rate_matrix_path="data/rate_matrices/equ.txt",  # TODO: Add EQU to GitHub!
+                num_rate_categories=20,
+                output_tree_dir=output_tree_dir,
+                num_processes=num_processes,
+            )
+            for protein_family_name in families:
+                tree_true_path = f"tests/test_input_data/trees_small_Q1_uniform/{protein_family_name}.newick"
+                tree_inferred_path = os.path.join(
+                    output_tree_dir, protein_family_name + ".newick"
+                )
+                l1_error = branch_length_l1_error(
+                    tree_true_path, tree_inferred_path
+                )
+                assert l1_error < 0.02  # Redundant, but just in case
 
-    # @parameterized.expand([("multiprocess", 3), ("single-process", 1)])
-    # def test_custom_rate_matrix_unnormalized_runs_regression(self, name, n_process):
-    #     """
-    #     Tests the use of an UNNORMALIZED custom rate matrix in FastTree.
-    #     """
-    #     with tempfile.TemporaryDirectory() as root_dir:
-    #         outdir = os.path.join(root_dir, 'trees_small_Q1_uniform_halved')
-    #         phylogeny_generator = PhylogenyGenerator(
-    #             a3m_dir_full='test_input_data/a3m_small',
-    #             a3m_dir='test_input_data/a3m_small',
-    #             n_process=n_process,
-    #             expected_number_of_MSAs=3,
-    #             outdir=outdir,
-    #             max_seqs=8,
-    #             max_sites=16,
-    #             max_families=3,
-    #             rate_matrix='input_data/synthetic_rate_matrices/Q1_uniform_halved_FastTree.txt',
-    #             fast_tree_cats=20,
-    #             use_cached=False,
-    #         )
-    #         phylogeny_generator.run()
-    #         for protein_family_name in ['1e7l_1_A', '5a0l_1_A', '6anz_1_B']:
-    #             tree_true_path = f"test_input_data/trees_small_Q1_uniform_halved/{protein_family_name}.newick"
-    #             tree_inferred_path = os.path.join(outdir, protein_family_name + '.newick')
-    #             l1_error = branch_length_l1_error(tree_true_path, tree_inferred_path)
-    #             assert(l1_error < 0.02)  # Redundant, but just in case
+    @parameterized.expand(
+        [("single-process", 1)]
+    )  # TODO: Test multiprocess too
+    def test_custom_rate_matrix_unnormalized_runs_regression(
+        self, name, num_processes
+    ):
+        """
+        Tests the use of an UNNORMALIZED custom rate matrix in FastTree.
+        """
+        with tempfile.TemporaryDirectory() as output_tree_dir:
+            # output_tree_dir = "fast_tree_trees_small"
+            families = ["1e7l_1_A", "5a0l_1_A", "6anz_1_B"]
+            fast_tree(
+                msa_dir="tests/test_input_data/a3m_small_fast_tree",
+                families=families,
+                rate_matrix_path="data/rate_matrices/equ_halved.txt",  # TODO: Add EQU_halved to GitHub!
+                num_rate_categories=20,
+                output_tree_dir=output_tree_dir,
+                num_processes=num_processes,
+            )
+            for protein_family_name in families:
+                tree_true_path = f"tests/test_input_data/trees_small_Q1_uniform_halved/{protein_family_name}.newick"
+                tree_inferred_path = os.path.join(
+                    output_tree_dir, protein_family_name + ".newick"
+                )
+                l1_error = branch_length_l1_error(
+                    tree_true_path, tree_inferred_path
+                )
+                assert l1_error < 0.02  # Redundant, but just in case
 
-    # @parameterized.expand([("multiprocess", 3), ("single-process", 1)])
-    # def test_inexistent_rate_matrix_raises_error(self, name, n_process):
-    #     """
-    #     If the rate matrix passed to FastTree does not exist, we should error out.
-    #     """
-    #     with tempfile.TemporaryDirectory() as root_dir:
-    #         outdir = os.path.join(root_dir, 'trees')
-    #         phylogeny_generator = PhylogenyGenerator(
-    #             a3m_dir_full='test_input_data/a3m_small',
-    #             a3m_dir='test_input_data/a3m_small',
-    #             n_process=n_process,
-    #             expected_number_of_MSAs=3,
-    #             outdir=outdir,
-    #             max_seqs=8,
-    #             max_sites=16,
-    #             max_families=3,
-    #             rate_matrix='I-do-not-exist',
-    #             fast_tree_cats=20,
-    #             use_cached=False,
-    #         )
-    #         with self.assertRaises(PhylogenyGeneratorError):
-    #             phylogeny_generator.run()
+    @parameterized.expand([("single-process", 1)])
+    def test_inexistent_rate_matrix_raises_error(self, name, num_processes):
+        """
+        If the rate matrix passed to FastTree does not exist, we should error out.
+        """
+        with tempfile.TemporaryDirectory() as output_tree_dir:
+            # output_tree_dir = "fast_tree_trees_small"
+            families = ["1e7l_1_A", "5a0l_1_A", "6anz_1_B"]
+            with self.assertRaises(Exception):
+                fast_tree(
+                    msa_dir="tests/test_input_data/a3m_small_fast_tree",
+                    families=families,
+                    rate_matrix_path="data/rate_matrices/i-do-not-exist.txt",
+                    num_rate_categories=20,
+                    output_tree_dir=output_tree_dir,
+                    num_processes=num_processes,
+                )
 
-    # @parameterized.expand([("multiprocess", 3), ("single-process", 1)])
-    # def test_malformed_a3m_file_raises_error(self, name, n_process):
-    #     """
-    #     If the a3m data is corrupted, an error should be raised.
-    #     """
-    #     with tempfile.TemporaryDirectory() as root_dir:
-    #         outdir = os.path.join(root_dir, 'trees')
-    #         phylogeny_generator = PhylogenyGenerator(
-    #             a3m_dir_full='test_input_data/a3m_small',
-    #             a3m_dir='test_input_data/a3m_small_corrupted',
-    #             n_process=n_process,
-    #             expected_number_of_MSAs=3,
-    #             outdir=outdir,
-    #             max_seqs=8,
-    #             max_sites=16,
-    #             max_families=3,
-    #             rate_matrix='None',
-    #             fast_tree_cats=20,
-    #             use_cached=False,
-    #         )
-    #         with self.assertRaises(MSAError):
-    #             phylogeny_generator.run()
-
-    # @parameterized.expand([("multiprocess", 3), ("single-process", 1)])
-    # def test_incorrect_expected_number_of_MSAs_raises_error(self, name, n_process):
-    #     """
-    #     If the a3m directory has a different number of files from the
-    #     expected number, an error should be raised.
-    #     """
-    #     with tempfile.TemporaryDirectory() as root_dir:
-    #         outdir = os.path.join(root_dir, 'trees')
-    #         phylogeny_generator = PhylogenyGenerator(
-    #             a3m_dir_full='test_input_data/a3m_small',
-    #             a3m_dir='test_input_data/a3m_small',
-    #             n_process=n_process,
-    #             expected_number_of_MSAs=4,
-    #             outdir=outdir,
-    #             max_seqs=8,
-    #             max_sites=16,
-    #             max_families=3,
-    #             rate_matrix='None',
-    #             fast_tree_cats=20,
-    #             use_cached=False,
-    #         )
-    #         with self.assertRaises(ValueError):
-    #             phylogeny_generator.run()
+    @parameterized.expand([("single-process", 1)])
+    def test_malformed_a3m_file_raises_error(self, name, num_processes):
+        """
+        If the a3m data is corrupted, an error should be raised.
+        """
+        with tempfile.TemporaryDirectory() as output_tree_dir:
+            # output_tree_dir = "fast_tree_trees_small"
+            families = ["1e7l_1_A", "5a0l_1_A", "6anz_1_B"]
+            with self.assertRaises(Exception):
+                fast_tree(
+                    msa_dir="tests/test_input_data/a3m_small_fast_tree_corrupted",
+                    families=families,
+                    rate_matrix_path="data/rate_matrices/jtt.txt",
+                    num_rate_categories=20,
+                    output_tree_dir=output_tree_dir,
+                    num_processes=num_processes,
+                )
