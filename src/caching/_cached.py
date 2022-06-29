44c46080c4cb7c2963f6e91bc4428299de1f16b1
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2022-06-28 23:26:24 -0700

    Must check "if exclude_if_default is not None:"

--- src/caching/_cached.py
+++ src/caching/_cached.py
@@ -62,15 +62,16 @@ def cached(
             # None of the exclude_if_default args can be a prefix of another one
             # (otherwise adversarial hash collisions can be easily crafted due
             # to how args are concatenated and hashed)
-            for arg1 in exclude_if_default:
-                for arg2 in exclude_if_default:
-                    if arg1 != arg2 and arg2.startswith(arg1):
-                        raise CacheUsageError(
-                            f"None of the exclude_if_default arguments can be a"
-                            f" prefix of another exclude_if_default argument."
-                            f" This ensures that it is not possible to craft"
-                            f" adversarial hash collisions."
-                        )
+            if exclude_if_default is not None:
+                for arg1 in exclude_if_default:
+                    for arg2 in exclude_if_default:
+                        if arg1 != arg2 and arg2.startswith(arg1):
+                            raise CacheUsageError(
+                                "None of the exclude_if_default arguments can "
+                                "be a prefix of another exclude_if_default "
+                                "argument. This ensures that it is not "
+                                "possible to craft adversarial hash collisions."
+                            )
 
             def excluded(arg, val) -> bool:
                 if exclude is not None and arg in exclude:
